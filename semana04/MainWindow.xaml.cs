66687c6b4363412e419aac44d806a0ac6a552ba2
Anthony <arosado-26@hotmail.com>
2020-04-20 23:32:15 -0500

    Agregar archivos de proyecto.

---
+++ semana04/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Buisness;
+using Entity;
+
+namespace semana04
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            BPedido bPedido = null;
+            try
+            {
+                bPedido = new BPedido();
+                dgvPedido.ItemsSource = bPedido.GetPedidosEntreFechas(Convert.ToDateTime(txtFechaInicio.Text),
+                    Convert.ToDateTime(txtFechaFin.Text));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el administrador.");
+            }
+            finally
+            {
+                bPedido = null;
+            }
+        }
+    }
+}
